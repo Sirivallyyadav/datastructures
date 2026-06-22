4f7c2cd66f035f3fb0c0fb0ce40861a6ecaf0d00
unknown <sirivallyyadav@gmail.com>
2026-06-22 15:33:13 +0530

    first commit

--- linked list.py
+++ linked list.py
@@ -1,11 +1,11 @@
 class Node:
     def __init__(self,data):
-       self.data=  data
-       self.next = None
-class Linkedlist :
+        self.data = data
+        self.next = None
+class LinkedList:
     def __init__(self):
         self.head = None
-    def add_end(self,data):
+    def add_end(self, data):
         new = Node(data)
         if self.head is None:
             self.head = new
@@ -13,18 +13,27 @@ class Linkedlist :
         itr = self.head
         while itr.next:
             itr = itr.next
-        itr.next = new 
+        itr.next = new
+    def add_beg(self,data):
+        obj = Node(data)
+        if self.head is None:
+            self.head = obj
+            return
+        obj.next = self.head
+        self.head = obj
+        
     def display(self):
         itr = self.head
         while itr:
-            print(itr.data,end='-->')
-            itr =itr.next
-l1 = Linkedlist()
-l1.add_end(50)
-l1.add_end(150)
-l1.add_end(250)
-l1.add_end(350)
-l1.add_end(450)
-l1.add_end(550)
-l1.add_end(650)
-l1.display()
+            print(itr.data,end="-->")
+            itr = itr.next
+ll = LinkedList()
+ll.add_end(50)
+ll.add_end(150)
+ll.add_end(250)
+ll.add_end(350)
+ll.add_end(450)
+ll.add_end(550)
+ll.add_end(650)
+ll.add_beg(10)
+ll.display()
